25bb541d0ed4dd08a6af3ff301b3154d80ad0e3c
DuongLT <duonglt.work@gmail.com>
2022-09-16 12:54:57 +0700

    update show list tours

--- chua_b1/Models/Tour.php
+++ chua_b1/Models/Tour.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'Models/db.php';
 
-function getTours() {
-    $sql = 'SELECT tours.id, tours.name, categories.name as cate_name FROM tours
-        LEFT JOIN categories
-        ON tours.category_id = categories.id
+function getTours()
+{
+    $sql = 'SELECT tours.id, tours.name,tours.img, tours.price, tours.description, tours.number_date, categories.name as cate_name 
+    FROM tours
+    LEFT JOIN categories
+    ON tours.category_id = categories.id
     ';
 
     return getData($sql, FETCH_ALL);
