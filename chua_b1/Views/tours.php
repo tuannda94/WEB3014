2365296c0096394db71a9e3b09576de86e175d04
thanhtd2000 <thanhhandgun2@gmail.com>
2022-09-16 15:38:44 +0700

    update request

--- chua_b1/Views/tours.php
+++ chua_b1/Views/tours.php
@@ -1,22 +1,30 @@
-<?php
-// Đã có biến $tours từ controller trước khi include view này
-echo '<pre>';
-var_dump($tours);
-echo '</pre>';
-?>
+
 <table>
     <thead>
         <td>ID</td>
         <td>Name</td>
         <td>Category</td>
+        <td>image</td>
+        <td>intro</td>
+        <td>description</td>
+        <td>number_date</td>
+        <td>price</td>
+
     </thead>
     <tbody>
-        <?php foreach ($tours as $key => $tour): ?>
+        <?php foreach ($tours as $key => $tour) : ?>
             <tr>
                 <td><?= $tour['id'] ?></td>
                 <td><?= $tour['name'] ?></td>
                 <td><?= $tour['cate_name'] ?></td>
+                <td><img src="img/<?= $tour['image'] ?>" alt="" width="50"></td>
+                <td><?= $tour['intro'] ?></td>
+                <td><?= $tour['description'] ?></td>
+                <td><?= $tour['number_date'] ?></td>
+                <td><?= $tour['price'] ?></td>
+                <td><a href="index.php?url=edit-tour&&id=<?= $tour['id'] ?>">Sửa</a> || <a href="index.php?url=delete-tour&&id=<?= $tour['id'] ?>">Xoá</a></td>
             </tr>
         <?php endforeach ?>
     </tbody>
 </table>
+<a href="index.php?url=create-tour">Thêm mới</a>
